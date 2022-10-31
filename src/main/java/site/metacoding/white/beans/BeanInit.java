53bd4a97b0ec4bcf2e7b5d340c3a9c87ed9b370f
Lee-na-yun <happyin3333@nate.com>
2022-11-01 00:01:11 +0900

    회원가입 Junit 통합테스트 완료

---
+++ src/main/java/site/metacoding/white/beans/BeanInit.java
@@ -0,0 +1,24 @@
+package site.metacoding.white.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@Configuration
+public class BeanInit{
+    
+    @Bean
+    public ObjectMapper omInit(){
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public HttpHeaders hhInit(){
+        return new HttpHeaders();
+    }
+}
